1303d178dcdcc9dd720c0a658905f1fe3bae41c3
DrBanner97 <arn884770@gmail.com>
2018-09-03 21:06:40 +0530

    00001336_000.png added

--- filter.py
+++ filter.py
@@ -1,8 +1,9 @@
 import csv
  
-with open('4999_images.csv', 'rt') as inp, open('4999_images_edit.csv', 'wt') as out:
+with open('/media/aditya/Windows/Users/shant/Pictures/data/Data_Entry_2017.csv', 'rt') as inp, open('4177_single_diseases.csv', 'at') as out:
     writer = csv.writer(out)
     for row in csv.reader(inp):
-        if row[1].find("|") == -1:
+        if row[1].find("00001336_000.png") != -1:
         	print(row[1])
         	writer.writerow(row)
+        	break
